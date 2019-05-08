5560a38c5594ca6ad357bc3bb20d216500397fa5
wlqwlq620 <wlqwlq620@users.noreply.github.com>
2019-05-08 16:19:41 +0800

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int i=0;
+	//scanf("%d",&i);
+	printf("My name is harry!\n");
+	return 0;
+}
---
+++ new.cpp
@@ -0,0 +1,15 @@
+#include<cstdio>
+#include<iostream>
+using namespace std;
+int main(){
+ 
+	int a,b;
+	scanf("%d",&a);
+	printf("%d HelloWorld!\n",a);
+ 
+	cin>>b;
+	cout<<b<<" HelloWorld!"<<endl;
+ 
+ 
+}
+
